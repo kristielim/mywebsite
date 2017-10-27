bb965a4136315802194f5713efaf6a8650792f54
unknown <kristie.lim@gmail.com>
2017-10-27 12:38:37 -0700

    fix navbar, align fade in

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 var isMobile;
+var FADE_TIME = 500;
 
 function attachEventHandlers() {
 	// TODO
 	$('.img-proj').on('click', handleImageClick); // when a div with the class image is clicked, do the function handleImageClick
 	$('ul.navbar-nav.nav').on('click', function() {
-		$('#myNavbar').addClass('collapsing').removeClass('collapse in').attr('aria-expanded', false);
+		if(isMobile.matches){
+			console.log("mobile");
+			$('#myNavbar').addClass('collapsing').removeClass('collapse in').attr('aria-expanded', false);
+		}
 	});
+}
+
+function changeText(label, text) {
+	label.hide();
+	label.text(text);
+	label.fadeIn(FADE_TIME)
 }
 
 function handleImageClick(event) {
@@ -24,22 +34,18 @@ function handleImageClick(event) {
 	const imgSrc = target.attr('src');
 	const sources = ['ytplaylist.PNG','hangman.png','fbmessages.PNG'];
 	const names = ['Youtube Playlist','Hangman Game','Messenger Reader'];
-	const descriptions = ['This Youtube playlist is made from every message ever sent to my Facebook Messenger account. It is based off tutorial from Julian Knodt and uses NodeJS.',
+	const descriptions = ['This Youtube playlist is made from every video ever sent to my Facebook Messenger account. It is based off tutorial from Julian Knodt and uses NodeJS.',
 							'This is a Harry Potter themed Hangman game, my final group project in AP Computer Science. It implements a GUI and uses Java.',
 							'This is a program to filter messages from the Facebook messenger archive by person and date, in chronological or reverse chronological order. It uses NodeJS.']
 	const links = ["https://goo.gl/dr76Pg","https://github.com/alveerak/hangman","https://goo.gl/pxypWU"];
 
-	const FADE_TIME = 400;
 	// TODO
 	if(target.hasClass('selected')) {
 		target.removeClass('selected');
 		images.removeClass('not-selected');
-		descLabel.fadeOut(FADE_TIME);
-		nameLabel.fadeOut(FADE_TIME, function() {
-			nameLabel.text("Click on a bubble!");
-			nameLabel.fadeIn(FADE_TIME);
-		});
-		linkLabel.fadeOut(FADE_TIME);
+		changeText(nameLabel,'Click on a bubble!');
+		descLabel.hide();
+		linkLabel.hide();
 
 	} else {
 		images.removeClass('selected');
@@ -50,18 +56,11 @@ function handleImageClick(event) {
 
 		const index = sources.indexOf(imgSrc);
 
-		nameLabel.fadeOut(FADE_TIME, function() {
-			nameLabel.text(names[index]);
-			nameLabel.fadeIn(FADE_TIME);
-		});
-		descLabel.fadeOut(FADE_TIME, function() {
-			descLabel.text(descriptions[index]);
-			descLabel.fadeIn(FADE_TIME);
-		});
-		linkLabel.fadeOut(FADE_TIME, function() {
-			linkLabel.attr('href',links[index]);
-			linkLabel.fadeIn(FADE_TIME);
-		});
+		changeText(nameLabel,names[index]);
+		changeText(descLabel,descriptions[index]);
+		linkLabel.hide();
+		linkLabel.attr('href',links[index]);
+		linkLabel.fadeIn(FADE_TIME);
 
 		if(isMobile.matches) {
 			$('html, body').animate({
